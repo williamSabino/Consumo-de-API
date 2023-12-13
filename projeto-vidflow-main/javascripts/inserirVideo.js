8af190beeeb72b207ec226b8206d4e5d1d65df91
Willi_sabino <williamsabino64@gmail.com>
2023-12-13 20:56:18 -0300

    Inserir finalizado

--- projeto-vidflow-main/javascripts/inserirVideo.js
+++ projeto-vidflow-main/javascripts/inserirVideo.js
@@ -1,6 +1,6 @@
 
-async function inserirVideo(titulo, url, imagem, categoria){
-    const video = JSON.stringify({titulo, url, imagem, categoria});
+async function inserirVideo(titulo, descricao, url, imagem, categoria) {
+    const video = JSON.stringify({ titulo, descricao, url, imagem, categoria });
     const response = await fetch(`http://localhost:3000/videos`, {
         method: "POST",
         headers: {
@@ -8,18 +8,24 @@ async function inserirVideo(titulo, url, imagem, categoria){
         },
         body: video,
     });
- return await response.json();
+    return await response.json();
 };
 
 const formularioEnviarVideo = document.querySelector('.formulario__inserirVideo');
+const btnVoltar = document.querySelector('input[value="Voltar"]');
 
-formularioEnviarVideo.addEventListener('submit', async(event)=>{
+btnVoltar.addEventListener('click', () => {
+    window.location.href = "../index.html";
+});
+
+formularioEnviarVideo.addEventListener('submit', async (event) => {
     event.preventDefault();
+    const visualizacao = Math.floor(Math.random() * 10);
+    const descricao = `${visualizacao} mil visualizações`;
     const titulo = document.querySelector('[data-titulo]').value;
     const url = document.querySelector('[data-url]').value;
     const imagem = document.querySelector('[data-imagem]').value;
     const categoria = document.querySelector('[data-categoria]').value;
-    console.log(titulo, url, imagem, categoria);
-    await inserirVideo(titulo, url, imagem, categoria);
+    await inserirVideo(titulo, descricao, url, imagem, categoria);
     window.location.href = "../index.html";
 });
